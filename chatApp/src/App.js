90cc1b4022040c9baaad8be96f952543261f2a5e
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-17 23:28:47 +0530

    basic setup firebase

--- chatApp/src/App.js
+++ chatApp/src/App.js
@@ -1,10 +1,6 @@
 
 import './App.css';
 
-
-
-
-
 function App() {
 
   return (
@@ -13,11 +9,16 @@ function App() {
     </div>
     )
       
-      
-      
-      
-     
-  
 }
 
 export default App;
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDu9HuUzJ4CMiVGrgeJi7otzJwHTxuVg1U",
+//   authDomain: "firechat-98288.firebaseapp.com",
+//   projectId: "firechat-98288",
+//   storageBucket: "firechat-98288.appspot.com",
+//   messagingSenderId: "727598954685",
+//   appId: "1:727598954685:web:d58641c40b4e347d4edfd4",
+//   measurementId: "G-SHEG3WBREG"
+// };
