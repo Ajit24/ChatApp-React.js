74b8e6c8359f6d35deebd097b4364a109d11f861
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-08-17 23:08:07 +0530

    Initialize project using Create-React-App

---
+++ chatApp/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>chat-app</h2>
+    </div>
+    )
+      
+      
+      
+      
+     
+  
+}
+
+export default App;
